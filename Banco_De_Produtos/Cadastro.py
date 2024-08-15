8000d012a14197e194f3c65c798f182eb3596347
NickolasFerraz1 <nickolas.oferraz@gmail.com>
2024-08-15 11:34:02 -0300

    Co-authored-by: Sandron Oliveira Silva <Sandron61@users.noreply.github.com>

--- Banco_De_Produtos/Cadastro.py
+++ Banco_De_Produtos/Cadastro.py
@@ -1,5 +1,24 @@
 from main import Produto
 
-class Cadastro:
+class Estoque(Produto):
     def __init__(self):
+        self.id_counter = 0
+
+    def create(self, nome, marca, categoria, qtd_estoque, preco, desc, fornecedor):
+        produto = Produto(self.id_counter, nome, marca, categoria, qtd_estoque, preco, desc, fornecedor)
+        self.id_counter += 1
+        id = self.id_counter
+        with open ('arquivo.txt', 'a') as file:
+            file.write(f"{id} | {produto.nome}| {produto.marca} | {produto.categoria} | {produto.qtd_estoque} | {produto.preco} | {produto.desc} | {produto.fornecedor} ")
+
+
+
+estoque = Estoque()
+
+# Criar um produto usando o método create
+estoque.create('Produto A', 'Marca A', 'Categoria A', 10, 99.99, 'Descrição do Produto A', 'Fornecedor A')
+
+
+                                        
+
         
--- Banco_De_Produtos/main.py
+++ Banco_De_Produtos/main.py
@@ -1,7 +1,8 @@
 class Produto:
-    def __init__(self, id_produto, nome, categoria, qtd_estoque, preco, desc, fornecedor):
+    def __init__(self, id_produto, nome, marca, categoria, qtd_estoque, preco, desc, fornecedor):
         self.id_produto = id_produto
         self.nome = nome
+        self.marca = marca
         self.categoria = categoria
         self.qtd_estoque = qtd_estoque
         self.preco = preco
